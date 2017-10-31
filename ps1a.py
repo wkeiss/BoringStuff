93a4a30274009752dc8f84e6031e55773ec9f0b4
wkeiss <{liguiguihua@gmail.com}>
2017-10-31 20:13:06 +0800

    update ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -1,17 +1,18 @@
 # Write a program that computes and prints the 1000th prime number.
 x = 2
 y = 2
-z = []
+z = [ ]
 
 if x >= y and x <= 1000:
-    if x / y == x / y * y:
-        z = z.apped[x / y]
+    if x == x / y * y:
+        z.append(x / y)
         if len(z) == 1:
             print x
             x = x + 1
+
         else:
             past
     else:
         y = y + 1
 else:
-    x = x + 1
+    past
