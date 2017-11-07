564b6806c43ae495001ec53f4a87f5d42f24acdb
wkeiss <{liguiguihua@gmail.com}>
2017-11-07 22:20:22 +0800

    update ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -1,12 +1,10 @@
 # Write a program that computes and prints the 1000th prime number.
-prime = []
+x = 3
 
-for x in range(3, 7920):
+while x != 1000:
     for y in range(2, x):
-        if x % y == 0:
-            x = x + 1
+        if  x % y == 0:
+            break
         else:
-            prime.append(x)
-
-print "The first 1,000 primes:"
-print prime
+            print x
+    x = x + 1
