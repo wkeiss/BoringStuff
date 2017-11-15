db6e0553472710fad34e7f560a1620cddfb5f1e8
wkeiss <{liguiguihua@gmail.com}>
2017-11-15 21:52:46 +0800

    add ps2a.py

---
+++ ps2a.py
@@ -0,0 +1,10 @@
+McNuqqets = [50, 51, 52, 53, 54, 55]
+
+for n in McNuqqets:
+    for a in range(0, 10):
+        for b in range(0, 7):
+            for c in range(0, 3):
+                if 6a + 9b + 20c == n：
+                    print "a = %d" % a, "b = %d" % b,"c = %d" % c,"n = %d" % n
+                else:
+                    break
