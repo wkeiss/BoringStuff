9ec8b599fd3070aafe5a4f8361412107979680b9
wkeiss <{liguiguihua@gmail.com}>
2017-11-23 23:16:39 +0800

    update ps3a.py

--- ps3a.py
+++ ps3a.py
@@ -4,10 +4,10 @@ def countSubStringMatch(target, key):
     a = find(target, key)
     b = len(key)
     count = 0
-    while a != -1:
-        index  = a + b
+    while a == -1:
+        index = a + b
         a = find(target, key, index)
-    count += 1
+        count += 1
     print "The key string presents %d time/times in target string." % count
 
 t = raw_input("please input your target string, which must contain double-qoute.")
